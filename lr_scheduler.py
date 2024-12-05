d53fd6edff9851a5c4936fbfb5091c642000ad31
Doun Lee <67096173+Ldoun@users.noreply.github.com>
2024-12-06 03:36:57 +0900

    Initial commit

---
+++ lr_scheduler.py
@@ -0,0 +1,34 @@
+from functools import partial
+import torch.optim.lr_scheduler as sch
+
+class base():
+    def __init__(self, *args):
+        self.optimizer = args[0]
+
+    def step(self):
+        pass
+
+    def get_last_lr(self):
+        return [param_group['lr'] for param_group in self.optimizer.param_groups]
+    
+class Warmup(sch._LRScheduler):
+    def __init__(self, optimizer, num_annealing_steps, num_total_steps):
+        self.num_annealing_steps = num_annealing_steps
+        self.num_total_steps = num_total_steps
+
+        super().__init__(optimizer)
+
+    def get_lr(self):
+        if self._step_count <= self.num_annealing_steps:
+            return [base_lr * self._step_count / self.num_annealing_steps for base_lr in self.base_lrs]
+        else:
+            return self.base_lrs
+
+
+def get_sch(scheduler, optimizer, **kwargs):
+    if scheduler=='None':
+        return base(optimizer)
+    elif scheduler=='warmup':
+        return Warmup(optimizer, num_annealing_steps=kwargs['warmup_epochs'], num_total_steps=kwargs['warmup_epochs'],)
+    else:
+        NotImplementedError(f'{scheduler} not implemented')
---
+++ utils.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import json
+import torch
+import random
+import numpy as np
+
+def seed_everything(seed: int):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
+    torch.use_deterministic_algorithms(True, warn_only=True)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+def handle_unhandled_exception(exc_type, exc_value, exc_traceback, logger=None):
+    if issubclass(exc_type, KeyboardInterrupt):
+                #Will call default excepthook
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        return
+        #Create a critical level log message with info from the except hook.
+    logger.critical("Unhandled exception", exc_info=(exc_type, exc_value, exc_traceback))
+
+def save_to_json(data, file_name):
+    with open(file_name, 'w') as fp:
+        json.dump(data, fp)
