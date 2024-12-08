4a95d49a4d7a458108090d93013cad67cf213223
Ldoun <ldoun8260@gmail.com>
2024-12-08 21:09:45 +0900

    fix: deterministic false

--- utils.py
+++ utils.py
@@ -13,7 +13,7 @@ def seed_everything(seed: int):
     torch.cuda.manual_seed(seed)
     os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
     torch.use_deterministic_algorithms(True, warn_only=True)
-    torch.backends.cudnn.deterministic = True
+    # torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
 def handle_unhandled_exception(exc_type, exc_value, exc_traceback, logger=None):
